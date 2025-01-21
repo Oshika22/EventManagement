fd79e57a60e38d89e9f907b4a24529bca267f927
Oshika22 <oshika004@gmail.com>
2025-01-21 23:09:26 +0530

    Merge branch 'main'

--- server/config.py
+++ server/config.py
@@ -1,4 +1,4 @@
 class Config:
-    MONGO_URI = "mongodb+srv://oshika1:tinker@cluster0.ukh05.mongodb.net/EventManagement"
+    MONGO_URI = "your database"
     UPLOAD_FOLDER = 'uploads'
     DEBUG = True
